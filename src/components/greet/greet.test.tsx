edae88f79a27ccd57bc2fdaa8091c3dac667bdc9
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-22 03:46:22 +0600

    testing last commit

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -4,11 +4,11 @@ import Greet from "./Greet";
 test("greet renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText('Hello');
-    expect(textElement).toBeInTheDocument()
+    expect(textElement).toBeInTheDocument();
 })
 
 test("greet renders with a name", () => {
     render(<Greet name="Shourov" />);
     const textElement = screen.getByText("Hello Shourov");
-    expect(textElement).toBeInTheDocument()
+    expect(textElement).toBeInTheDocument();
 })
