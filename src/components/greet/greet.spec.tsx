f77edff170c3f48de14ee9cedb0c918dd97bde78
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-22 23:40:45 +0600

    greet.spec

---
+++ src/components/greet/greet.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+// a -> w -> o
+// describe.only/skip
+
+describe("Greet", () => {
+    test("greet renders correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    })
+})
+
+
+describe("nested", () => {
+    test("greet renders with a name", () => {
+        render(<Greet name="Shourov" />);
+        const textElement = screen.getByText("Hello Shourov");
+        expect(textElement).toBeInTheDocument();
+    })
+})
+
+
