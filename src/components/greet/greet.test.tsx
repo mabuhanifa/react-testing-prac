a72be031330b6c4b7da924d211704f6dc408170f
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-22 23:28:22 +0600

    skip & only

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -4,13 +4,13 @@ import Greet from "./Greet";
 
 // a -> w -> o
 
-test("greet renders correctly", () => {
+test.skip("greet renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText('Hello');
     expect(textElement).toBeInTheDocument();
 })
 
-test("greet renders with a name", () => {
+test.only("greet renders with a name", () => {
     render(<Greet name="Shourov" />);
     const textElement = screen.getByText("Hello Shourov");
     expect(textElement).toBeInTheDocument();
